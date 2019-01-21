ab9679237f99512c3c20ff58f183579413464374
Schleucher Jonathan (IFD FE PD AMF TSIA) <Jonathan.Schleucher@infineon.com>
2019-01-21 11:16:29 +0100

    Assembly Info

--- FrequentlyUsedCode/Program.cs
+++ FrequentlyUsedCode/Program.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace FrequentlyUsedCode
 {
     class Program
     {
         static void Main(string[] args) {
+            var allNumbers = new List<int>() {1,2,3,4,5 };
+            var smallNumbers = allNumbers.Where(x => x<3);
+            var largeNumbers = from x in allNumbers where x > 3 select x;
+
+            foreach (var smallNumber in smallNumbers) {
+
+            }
         }
     }
 }
